c62cb82c585ccc8e4be724e9aadd6a89026a8074
Lokesh Goluguri <lokeshreddy470@gmail.com>
2020-03-24 09:49:59 -0500

    azure Vnet module

---
+++ azure-vnet-module/test/fixture/main.tf
@@ -0,0 +1,38 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "random_id" "rg_name" {
+  byte_length = 8
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "test-${random_id.rg_name.hex}-rg"
+  location = var.location
+}
+
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "test-${random_id.rg_name.hex}-nsg"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+}
+
+module "vnet" {
+  source              = "../../"
+  resource_group_name = azurerm_resource_group.test.name
+  address_space       = ["10.0.0.0/16"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  nsg_ids = {
+    subnet1 = azurerm_network_security_group.nsg1.id
+  }
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
+}
+
+
+
